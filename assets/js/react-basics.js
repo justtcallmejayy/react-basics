740d4e9704b132adc35230f6386e9abcb80598a2
JAY CHOKSI <jaychokxii1804@gmail.com>
2025-03-02 21:40:12 -0500

    New formated araay for values and suits

--- assets/js/react-basics.js
+++ assets/js/react-basics.js
@@ -1,5 +1,22 @@
 // StAuth10244: I Jay Choksi, 000887533 certify that this material is my original work. No other person's work has been used without due acknowledgement. I have not made my work available to anyone else.
 
-    // Constants for suits and values
-    const suits = ["♥", "♦", "♣", "♠"]; // Hearts, Diamonds, Clubs, Spades icons copied from requirments.
-    const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]; // Ace, 2-10, Jack, Queen, King all the cards values.
+// Constants for suits and values
+const suits = ["♥", "♦", "♣", "♠"]; // Hearts, Diamonds, Clubs, Spades icons copied from requirments.
+const values = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K",
+]; // Ace, 2-10, Jack, Queen, King all the cards values.
+
+// We can generate a class to create card object of 52 cards. Later a new deck can be created by creating an object of this class.
+//Then we can shuffle the deck and draw a card from the deck.. or create a new deck and shuffle it and draw a card.

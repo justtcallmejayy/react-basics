a3532ba648ed5c904b16b56fc0101851c19a1f92
JAY CHOKSI <jaychokxii1804@gmail.com>
2025-03-02 21:32:01 -0500

    Added the cards and its values

--- assets/js/react-basics.js
+++ assets/js/react-basics.js
@@ -1,1 +1,5 @@
 // StAuth10244: I Jay Choksi, 000887533 certify that this material is my original work. No other person's work has been used without due acknowledgement. I have not made my work available to anyone else.
+
+    // Constants for suits and values
+    const suits = ["♥", "♦", "♣", "♠"]; // Hearts, Diamonds, Clubs, Spades icons copied from requirments.
+    const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]; // Ace, 2-10, Jack, Queen, King all the cards values.
